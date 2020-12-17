03c97748c1546f4c170034171499ac91530901bd
tiago <tiagolemespalhano@gmail.com>
2020-12-17 00:37:46 -0300

    BLog showing the posts correctly.

--- cms/index.php
+++ cms/index.php
@@ -10,27 +10,67 @@
 
             <!-- Blog Entries Column -->
             <div class="col-md-8">
-
                 <h1 class="page-header">
                     Page Heading
                     <small>Secondary Text</small>
                 </h1>
 
-                <!-- First Blog Post -->
-                <h2>
-                    <a href="#">Blog Post Title</a>
-                </h2>
-                <p class="lead">
-                    by <a href="index.php">Start Bootstrap</a>
-                </p>
-                <p><span class="glyphicon glyphicon-time"></span> Posted on August 28, 2013 at 10:00 PM</p>
-                <hr>
-                <img class="img-responsive" src="http://placehold.it/900x300" alt="">
-                <hr>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore, veritatis, tempora, necessitatibus inventore nisi quam quia repellat ut tempore laborum possimus eum dicta id animi corrupti debitis ipsum officiis rerum.</p>
-                <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
+                <?php
 
-                <hr>
+                    $query = "SELECT * FROM posts ORDER BY post_id DESC";
+
+                    $select_all_posts_query = mysqli_query($connection, $query);
+
+                    while ($row = mysqli_fetch_assoc($select_all_posts_query)) {
+                        $post_title = utf8_encode($row['post_title']);
+                        $post_author = utf8_encode($row['post_author']);
+                        $post_date = utf8_encode($row['post_date']);
+                        $post_image = utf8_encode($row['post_image']);
+                        $post_content = utf8_encode($row['post_content']);
+
+                        ?>                    
+
+                        <!-- First Blog Post -->
+                        <h2>
+                            <a href="#"><?php echo $post_title; ?></a>
+                        </h2>
+
+                        <p class="lead">
+                            by <a href="index.php"><?php echo $post_author; ?></a>
+                        </p>
+
+                        <p>
+                            <span class="glyphicon glyphicon-time">
+                                
+                            </span> 
+                            Posted on <?php echo $post_date; ?>
+                        </p>
+
+                        <hr>
+
+                        <img class="img-responsive" src="http://placehold.it/900x300" alt="">
+
+                        <hr>
+
+                        <p>
+                            <?php echo $post_content; ?>
+                        </p>
+
+                        <a class="btn btn-primary" href="#">
+                            Read More 
+                            <span class="glyphicon glyphicon-chevron-right">
+                                
+                            </span>
+                        </a>
+
+                        <hr>
+
+                        <?php
+                    }
+
+                ?>
+
+                
 
             </div>
 
