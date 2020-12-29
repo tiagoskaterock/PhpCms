c4bc187a7e5c61cdcc0fc4f2973c043ecbaa7b3e
tiago <tiagolemespalhano@gmail.com>
2020-12-28 22:34:42 -0300

    Categorias podendo ser criadas ou deletadas do CMS.

--- cms/admin/categories.php
+++ cms/admin/categories.php
@@ -18,7 +18,31 @@
 
           <!-- LEFT -->
           <div class="col-xs-6">
-            <form action="">
+
+            <?php
+
+              if (isset($_POST['submit'])) {
+
+                $cat_title = $_POST['cat_title'];
+
+                if ($cat_title == "" || empty($cat_title)) {
+                  echo '<h3>This field should not be empty</h3>';
+                }
+
+                else{
+                  $query = "INSERT INTO `categories` (`cat_id`, `cat_title`) VALUES (NULL, '{$cat_title}'); ";
+
+                  $create_category_query = mysqli_query($connection, $query);
+
+                  if (!$create_category_query) {
+                    die('QUERY FAILED' . mysqli_error($connection));
+                  }
+                }
+              }
+
+            ?>
+
+            <form action="" method="post">
 
               <label for="cat_title">Add Category</label>
               <div class="form-group">
@@ -32,10 +56,7 @@
           </div>
           <!-- END LEFT -->
 
-          <?php
-            $query = "SELECT * FROM categories;";
-            $select_categories = mysqli_query($connection, $query);                  
-          ?>
+
 
           <!-- RIGHT -->
           <div class="col-xs-6">
@@ -48,14 +69,52 @@
               </thead>
 
               <tbody>              
-              <?php 
-              while ($row = mysqli_fetch_assoc($select_categories)) { ?>
-                  <tr>
-                    <td><?= $row['cat_id'] ?></td>
-                    <td><?= $row['cat_title'] ?></td>
-                  </tr>
                 <?php 
-              } ?>
+                  // FIND ALL CATEGORIES 
+                  $query = "SELECT * FROM categories;";
+                  $select_categories = mysqli_query($connection, $query);   
+
+                  while ($row = mysqli_fetch_assoc($select_categories)) { ?>
+                    <tr>
+                      <!-- ID -->
+                      <td><?= $row['cat_id'] ?></td>
+
+                      <!-- Categoria -->
+                      <td><?= $row['cat_title'] ?></td>                    
+
+                      <!-- Botão delete -->
+                      <td>
+                        <a href="categories.php?delete=<?= $row['cat_id'] ?>">
+                        Delete
+                        </a>
+                      </td> 
+
+                    </tr>
+                    <?php 
+                  }                   
+                ?>
+
+
+
+
+                  <?php
+                    // DELETE QUERY
+                    if (isset($_GET['delete'])) {
+
+                      $the_cat_id = $_GET['delete'];
+
+                      $query = "DELETE FROM categories WHERE cat_id = {$the_cat_id}";
+
+                      $delete_query = mysqli_query($connection, $query);
+
+                      // REFRESH
+                      header("Location: categories.php");
+                    }
+                    
+                  ?>
+
+
+
               </tbody>
 
             </table>
